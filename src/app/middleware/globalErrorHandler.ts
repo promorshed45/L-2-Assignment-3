1a6c235d7155fc4722319589db75774bc589887e
promorshed45 <ctgmorshed45@gmail.com>
2024-06-12 15:54:51 +0600

    project setup with needed package

---
+++ src/app/middleware/globalErrorHandler.ts
@@ -0,0 +1,61 @@
+import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
+import { ZodError } from "zod";
+import config from "../config";
+import handleZodError from "../errors/handleZodError";
+import { TErrorSources } from "../interface/error";
+import handleValidationError from "../errors/handleValidationError";
+import handleCastError from "../errors/handleCastError";
+import handleDuplicateError from "../errors/handleDuplicateError";
+import AppError from "../errors/AppError";
+
+const globalErrorHandler: ErrorRequestHandler = ((err, req, res, next) => {
+  let statusCode = 500;
+  let message = 'Something went wroing';
+
+  let errorSources: TErrorSources = [{
+    path: '',
+    message: 'Something went wroing'
+  }]
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources
+  } else if (err?.name === 'ValidatorError') {
+    const simplifiedError = handleValidationError(err)
+    statusCode = simplifiedError?.statusCode,
+      message = simplifiedError?.message,
+      errorSources = simplifiedError?.errorSources
+  } else if (err?.name === 'CastError') {
+    const simplifiedError = handleCastError(err)
+    statusCode = simplifiedError?.statusCode,
+      message = simplifiedError?.message,
+      errorSources = simplifiedError?.errorSources
+  } else if (err?.code === 11000) {
+    const simplifiedError = handleDuplicateError(err)
+    statusCode = simplifiedError?.statusCode,
+      message = simplifiedError?.message,
+      errorSources = simplifiedError?.errorSources
+  } else if (err instanceof Error) {
+    message = err?.message;
+    errorSources = [
+      {
+        path: '',
+        message: err?.message
+      }
+    ]
+
+    // ultmate return
+    return res.status(statusCode).json({
+      success: false,
+      message,
+      errorSources,
+      err,
+      stack: config.node_env === 'production' ? err?.stack : null
+    })
+  }
+});
+
+
+export default globalErrorHandler;
---
+++ src/app/routes/index.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+
+
+const router = Router();
+
+// const moduleRoutes = [
+//     {
+//         path: '/users',
+//         route: 
+//     },
+    
+//     {
+//         path: '/admin',
+//         route: 
+//     },
+//     {
+//         path: '/auth',
+//         route: ,  
+//     },
+// ]
+
+// moduleRoutes.forEach(route => router.use(route.path, route.route))
+
+
+export default router;
